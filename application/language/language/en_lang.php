8a53c9d24edcc93179828b757149bff73e2da81d
thinhlv <thinhluuvan@admicro.vn>
2019-01-03 17:18:46 +0700

    push full version

---
+++ application/language/language/en_lang.php
@@ -0,0 +1,39 @@
+<?php
+    $lang['fullname'] = "Fullname";
+    $lang['email'] = "Email";
+    $lang['birth'] = "Birthday";
+    $lang['sex'] = "Sex";
+    $lang['male'] = "Male";
+    $lang['female'] = "Female";
+    $lang['birthday'] = "Birthday";
+    $lang['feedback'] = "Feedback";
+    $lang['phone'] = "Phone";
+    $lang['address'] = "Address";
+    $lang['plhPhone'] = "Enter your phone";
+    $lang['plhBirthday'] = "Enter your birthday";
+    //menu
+    $lang['home'] = "Home";
+    $lang['about'] = "INTRODUCE";
+    $lang['recruit'] = 'CAREER';
+    $lang['contact'] = 'Contact us';
+    //footer
+    $lang['plhName'] = 'Enter your name';
+    $lang['plhEmail'] = 'Enter your email';
+    $lang['plhMessage'] = 'Enter your message';
+    $lang['btnSend'] = 'Send';
+    //home
+    $lang['service'] = 'Our services';
+    $lang['news'] = 'News - Event';
+    //new
+    $lang['style'] = 'SGC Style';
+    //recruit
+    $lang['subscribe'] = 'Subscribe';
+    $lang['department'] = 'Department';
+    $lang['technical'] = 'Technical';
+    $lang['business'] = 'Business';
+    $lang['tester'] = 'Tester';
+    $lang['opening'] = 'POSITIONS';
+    $lang['apply'] = 'Apply';
+    $lang['detail'] = 'Detail';
+    $lang['btnSubscribe'] = 'Subscribe';
+?>
