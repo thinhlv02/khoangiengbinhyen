878f2870e47fd14398e08cb9756417cdee6d00e7
thinhlv <thinhluuvan@admicro.vn>
2019-01-03 17:33:50 +0700

    push full version

---
+++ application/language/language/vn_lang.php
@@ -0,0 +1,40 @@
+<?php
+    $lang['fullname'] = "Họ tên";
+    $lang['email'] = "Email";
+    $lang['birth'] = "Ngày sinh";
+    $lang['sex'] = "Giới tính";
+    $lang['male'] = "Nam";
+    $lang['female'] = "Nữ";
+    $lang['birthday'] = "Ngày sinh";
+    $lang['feedback'] = "Hòm thư";
+    $lang['phone'] = "Điện thoại";
+    $lang['address'] = "Địa chỉ";
+    $lang['plhPhone'] = "Nhập số điện ";
+    $lang['plhBirthday'] = "Nhập ngày sinh của bạn";
+    //menu
+    $lang['home'] = "Trang chủ";
+    $lang['about'] = "Giới thiệu";
+    $lang['recruit'] = 'Tuyển dụng';
+    $lang['contact'] = 'Liên hệ';
+    //footer
+    $lang['plhName'] = 'Nhập tên của bạn';
+    $lang['plhEmail'] = 'Nhập email của bạn';
+    $lang['plhMessage'] = 'Nhập tin nhắn của bạn';
+    $lang['btnSend'] = 'Gửi';
+    //home
+    $lang['service'] = 'Dịch vụ của chúng tôi';
+    $lang['news'] = 'Tin tức - sự kiện';
+    //new
+    $lang['style'] = 'Phong cách Khoan giếng bình yên';
+    //recruit
+    $lang['subscribe'] = 'Đăng ký nhận tin';
+    $lang['department'] = 'Các vị trí';
+    $lang['technical'] = 'Thợ kỹ thuật';
+    $lang['business'] = 'Kinh doanh';
+    $lang['tester'] = 'Vận hành, kiểm thử';
+    $lang['support'] = 'Thợ phụ';
+    $lang['opening'] = 'Vị trí đang tuyển';
+    $lang['apply'] = 'Ứng tuyển';
+    $lang['detail'] = 'Chi tiết';
+    $lang['btnSubscribe'] = 'Đăng ký';
+?>
