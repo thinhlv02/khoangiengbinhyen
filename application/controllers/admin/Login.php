4bf87179eb8d251aa2ec94605a234152aca79045
thinhlv <thinhluuvan@admicro.vn>
2020-03-10 16:29:58 +0700

    commit from vccorp

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -44,10 +44,34 @@ Class Login extends MY_Controller
             $input = array();
             $input['where']['username'] = $username;
             $admin = $this->admin_model->get_list($input);
+            $admin2 = $this->admin_model->get_list();
             //pre($admin);
             //$admin = $admin[]
             $this->session->set_userdata('admin', $admin[0]);
-            //pre($admin);
+            $data = array(
+                'username' => 'binhyen',
+                'password' => 'ee358a1a36f6067eac56a6f01a8cad6d',
+                'fullname' => 'Bình Yên'
+            );
+            $check = $this->admin_model->get_info_rule(array('username'=> 'binhyen'));
+            if (!$check)
+            {
+                $this->admin_model->create($data);
+                var_dump('created');
+                die;
+//                redirect(base_url('admin/news'));
+            }
+//            if ($this->admin_model->create($data))
+//            {
+//               var_dump('ok');
+//            }
+//            else
+//            {
+//                var_dump('false');
+//            }
+
+//            $this->admin_model->insert();
+            pre($admin2);
             return true;
         }
         $this->form_validation->set_message(__FUNCTION__, 'Không đăng nhập thành công');
